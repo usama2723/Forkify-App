0e63a6f77942877023f3917ac8e57d73c8fd45fe
usama2723 <sammirza201@gmail.com>
2025-03-10 16:17:54 +0500

    node modules

---
+++ dist/index.62406edb.js
@@ -0,0 +1,39 @@
+const recipeContainer = document.querySelector('.recipe');
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+// NEW API URL (instead of the one shown in the video)
+// https://forkify-api.jonas.io
+///////////////////////////////////////
+const showRecipe = async function() {
+    try {
+        const response = await fetch('https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886');
+        const data = await response.json();
+        if (!response.ok) throw new error(`${data.message} (${response.status})`);
+        console.log(response, data);
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredints: recipe.ingredints
+        };
+        console.log(recipe);
+        const markup = `
+    
+    `;
+    } catch (error1) {
+        alert(error1);
+    }
+};
+showRecipe();
+
+//# sourceMappingURL=index.62406edb.js.map
--- src/js/controller.js
+++ src/js/controller.js
@@ -12,3 +12,36 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const response = await fetch('https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const data = await response.json();
+
+    if (!response.ok) throw new error(`${data.message} (${response.status})`)
+    console.log(response, data);
+
+    let { recipe } = data.data
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredints: recipe.ingredints,
+    }
+    console.log(recipe);
+
+
+    const markup = `
+    
+    `
+
+  }
+  catch (error) {
+    alert(error)
+  }
+}
+showRecipe();
